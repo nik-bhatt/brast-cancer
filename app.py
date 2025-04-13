b88c9353b4737743898b0e6ebe4c1148986e6171
nik-bhatt <nihar.bhatt0703@gmail.com>
2025-04-12 22:49:04 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+
+import pickle
+
+import numpy as np
+from flask import Flask, render_template, request
+
+# loading model
+model = pickle.load(open('module.pkl', 'rb'))
+
+# flask app
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    features = request.form['feature']
+    features = features.split(',')
+    np_features = np.asarray(features, dtype=np.float32)
+
+    # prediction
+    pred = model.predict(np_features.reshape(1, -1))
+    message = ['Cancrouse' if pred[0] == 1 else 'Not Cancrouse']
+    # print(message[0])
+    return render_template('index.html', message=message)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
